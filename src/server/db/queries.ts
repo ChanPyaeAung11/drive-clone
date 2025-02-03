5c7d6d0abb21885422cd0218f5f39bfa59183e6b
Chan Pyae Aung <chanpyaeaung@Chans-MacBook-Pro.local>
2025-02-02 20:12:30 -0800

    cleanup data access pattern

---
+++ src/server/db/queries.ts
@@ -0,0 +1,36 @@
+import {
+  files_table as filesSchema,
+  folders_table as foldersSchema,
+} from "~/server/db/schema";
+import { db } from "~/server/db";
+import { eq } from "drizzle-orm";
+
+export async function getAllParentsForFolder(folderId: number) {
+  const parents = [];
+  let currentId: number | null = folderId;
+  while (currentId !== null) {
+    const folder = await db
+      .selectDistinct()
+      .from(foldersSchema)
+      .where(eq(foldersSchema.id, currentId));
+
+    if (!folder[0]) {
+      throw new Error("Parent folder not found");
+    }
+    parents.unshift(folder[0]);
+    currentId = folder[0]?.parent;
+  }
+  return parents;
+}
+
+export function getFolders(folderId: number) {
+  // since i am returning a promise, no need to async/await. Node smart enough to infer from returning a promise
+  return db
+    .select()
+    .from(foldersSchema)
+    .where(eq(foldersSchema.parent, folderId));
+}
+
+export function getFiles(folderId: number) {
+  return db.select().from(filesSchema).where(eq(filesSchema.parent, folderId));
+}
